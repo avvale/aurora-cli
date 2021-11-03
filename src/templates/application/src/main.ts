54b7049cc5e4accaba1ce41b14004de657e0111c
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2021-11-04 00:14:01 +0100

    add swagger

--- src/templates/application/src/main.ts
+++ src/templates/application/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { setTimeZoneApplication } from 'aurora-ts-core';
 import { AppModule } from './app.module';
 import { urlencoded, json } from 'express';
@@ -18,6 +19,15 @@ async function bootstrap()
     const app           = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
 
+    // set swagger config
+    const options = new DocumentBuilder()
+        .setTitle('Hades API')
+        .setDescription('API to consume Hades services')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+
     // set limit of request allowed
     app.use(json({ limit: configService.get<string>('APP_LIMIT_REQUEST_SIZE') }));
     app.use(urlencoded({ extended: true, limit: configService.get<string>('APP_LIMIT_REQUEST_SIZE') }));
@@ -25,7 +35,7 @@ async function bootstrap()
     // set timezone application from .env APP_TIMEZONE on dayjs
     setTimeZoneApplication(configService, dayjs);
 
-    await app.listen(3000);
+    await app.listen(configService.get<number>('APP_PORT'));
 }
 
 bootstrap();
