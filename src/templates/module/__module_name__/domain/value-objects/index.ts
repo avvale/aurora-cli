8f88cbd2eb497bfe9854f30c102c72b23497e772
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2021-11-22 09:45:57 +0100

    wip

--- src/templates/module/__module_name__/domain/value-objects/index.ts
+++ src/templates/module/__module_name__/domain/value-objects/index.ts
@@ -1,5 +1,5 @@
 {{#each schema.properties.valueObjects}}
 {{#if (allowProperty ../schema.moduleName this) }}
-export { {{ toPascalCase ../schema.moduleName }}{{> i18n }}{{ toPascalCase name }} } from './{{ toKebabCase ../schema.moduleName }}-{{ toKebabCase name }}';
+export { {{ toPascalCase ../schema.moduleName }}{{> i18n }}{{ toPascalCase name }} } from './{{ toKebabCase ../schema.moduleName }}{{#if isI18n }}-i18n{{/if}}-{{ toKebabCase name }}';
 {{/if}}
 {{/each}}
