91f6fccaa2f2da40f84d7da230d91b109a6cc26a
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2021-11-25 12:42:46 +0100

    wip

--- src/templates/api/__module_name__/controllers/__bounded_context_name__-find-__module_name__.controller.ts
+++ src/templates/api/__module_name__/controllers/__bounded_context_name__-find-__module_name__.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Body{{#if schema.hasOAuth}}, UseGuards{{/if}} } from '@nestjs/common';
+import { Controller, Get, Body{{#if schema.hasOAuth}}, UseGuards{{/if}}{{#if schema.properties.hasI18n}}, Headers{{/if}} } from '@nestjs/common';
 import { ApiTags, ApiOkResponse, ApiOperation, ApiBody, ApiQuery } from '@nestjs/swagger';
 import { QueryStatement, Timezone } from '{{ config.auroraCorePackage }}';
 import { {{ toPascalCase schema.moduleName }}Dto } from './../dto/{{ toKebabCase schema.moduleName }}.dto';
@@ -20,6 +20,9 @@ import { CurrentAccount } from './../../../shared/decorators/current-account.dec
 // {{ config.applicationsContainer }}
 import { IQueryBus } from '{{ config.auroraLocalPackage }}/cqrs/domain/query-bus';
 import { Find{{ toPascalCase schema.moduleName }}Query } from '{{ config.applicationsContainer }}/{{ toKebabCase schema.boundedContextName }}/{{ toKebabCase schema.moduleName }}/application/find/find-{{ toKebabCase schema.moduleName }}.query';
+{{#if schema.properties.hasI18n}}
+import { AddI18NConstraintService } from '@apps/common/lang/application/lib/add-i18n-constraint.service';
+{{/if}}
 
 @ApiTags('[{{ toKebabCase schema.boundedContextName }}] {{ toKebabCase schema.moduleName }}')
 @Controller('{{ toKebabCase schema.boundedContextName }}/{{ toKebabCase schema.moduleName }}')
@@ -31,6 +34,9 @@ export class {{ toPascalCase schema.boundedContextName }}Find{{ toPascalCase sch
 {
     constructor(
         private readonly queryBus: IQueryBus,
+        {{#if schema.properties.hasI18n}}
+        private readonly addI18NConstraintService: AddI18NConstraintService,
+        {{/if}}
     ) {}
 
     @Get()
@@ -45,11 +51,17 @@ export class {{ toPascalCase schema.boundedContextName }}Find{{ toPascalCase sch
         {{#if schema.hasTenant}}
         @CurrentAccount() account: AccountResponse,
         {{/if}}
+        {{#if schema.properties.hasI18n}}
+        @Headers('Content-Language') contentLanguage?: string,
+        {{/if}}
         @Body('query') queryStatement?: QueryStatement,
         @Body('constraint') constraint?: QueryStatement,
         @Timezone() timezone?: string,
     )
     {
+        {{#if schema.properties.hasI18n}}
+        constraint = await this.addI18NConstraintService.main(constraint, '{{ toCamelCase schema.moduleName }}I18N', contentLanguage);
+        {{/if}}
         return await this.queryBus.ask(new Find{{ toPascalCase schema.moduleName }}Query(queryStatement, constraint, { timezone }));
     }
 }
--- src/templates/module/__module_name__/application/create/create-__module_names__.service.ts
+++ src/templates/module/__module_name__/application/create/create-__module_names__.service.ts
@@ -58,7 +58,7 @@ export class Create{{ toPascalCase schema.moduleNames }}Service
         // insert
         {{#if schema.properties.hasI18n}}
         // delete duplicate elements from multiple languages
-        await this.repository.insert(aggregate{{ toPascalCase schema.moduleNames }}.filter((country, index, self) => index === self.findIndex(t => (t.id === country.id))));
+        await this.repository.insert(aggregate{{ toPascalCase schema.moduleNames }}.filter((country, index, self) => index === self.findIndex(t => t.id.value === country.id.value)));
         await this.repositoryI18n.insert(aggregate{{ toPascalCase schema.moduleNames }}, {}, aggregate => aggregate.toI18nDTO());
         {{else}}
         await this.repository.insert(aggregate{{ toPascalCase schema.moduleNames }});
