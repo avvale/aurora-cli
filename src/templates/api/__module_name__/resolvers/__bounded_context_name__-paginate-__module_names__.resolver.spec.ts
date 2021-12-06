151d3853cdc07cdfbf45ccdc890a0ead20551c06
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2021-12-06 19:35:44 +0100

    wip

--- src/templates/api/__module_name__/resolvers/__bounded_context_name__-paginate-__module_names__.resolver.spec.ts
+++ src/templates/api/__module_name__/resolvers/__bounded_context_name__-paginate-__module_names__.resolver.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Test, TestingModule } from '@nestjs/testing';
 {{#if schema.properties.hasI18n}}
 import { CacheModule } from '@nestjs/common';
