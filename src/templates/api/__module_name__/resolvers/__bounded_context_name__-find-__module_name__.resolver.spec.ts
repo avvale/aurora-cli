b43f3814a8a2256f560f51d82973193192b99340
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2021-12-06 19:33:50 +0100

    wip

--- src/templates/api/__module_name__/resolvers/__bounded_context_name__-find-__module_name__.resolver.spec.ts
+++ src/templates/api/__module_name__/resolvers/__bounded_context_name__-find-__module_name__.resolver.spec.ts
@@ -1,9 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Test, TestingModule } from '@nestjs/testing';
+{{#if schema.properties.hasI18n}}
+import { CacheModule } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+{{/if}}
 
 // custom items
 import { {{ toPascalCase schema.boundedContextName }}Find{{ toPascalCase schema.moduleName }}Resolver } from './{{ toKebabCase schema.boundedContextName }}-find-{{ toKebabCase schema.moduleName }}.resolver';
 import { ICommandBus } from '{{ config.auroraLocalPackage }}/cqrs/domain/command-bus';
 import { IQueryBus } from '{{ config.auroraLocalPackage }}/cqrs/domain/query-bus';
+{{#if schema.properties.hasI18n}}
+import { AddI18NConstraintService } from '@apps/common/lang/application/shared/add-i18n-constraint.service';
+import { GetLangsCacheService } from '@apps/common/lang/application/shared/get-langs-cache.service';
+{{/if}}
+
+// sources
+{{#if schema.properties.hasI18n}}
+import { langs } from '@apps/common/lang/infrastructure/seeds/lang.seed';
+{{/if}}
 import { {{ toCamelCase schema.moduleNames }} } from '{{ config.applicationsContainer }}/{{ toKebabCase schema.boundedContextName }}/{{ toKebabCase schema.moduleName }}/infrastructure/seeds/{{ toKebabCase schema.moduleName }}.seed';
 
 describe('{{ toPascalCase schema.boundedContextName }}Find{{ toPascalCase schema.moduleName }}Resolver', () =>
@@ -15,8 +29,28 @@ describe('{{ toPascalCase schema.boundedContextName }}Find{{ toPascalCase schema
     beforeAll(async () =>
     {
         const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                {{#if schema.properties.hasI18n}}
+                CacheModule.register(),
+                {{/if}}
+            ],
             providers: [
                 {{ toPascalCase schema.boundedContextName }}Find{{ toPascalCase schema.moduleName }}Resolver,
+                {{#if schema.properties.hasI18n}}
+                AddI18NConstraintService,
+                {
+                    provide : ConfigService,
+                    useValue: {
+                        get: (key: string) => key === 'APP_LANG' ? 'es' : ''
+                    }
+                },
+                {
+                    provide : GetLangsCacheService,
+                    useValue: {
+                        main: () => langs,
+                    }
+                },
+                {{/if}}
                 {
                     provide : IQueryBus,
                     useValue: {
--- src/templates/api/__module_name__/resolvers/__bounded_context_name__-update-__module_name__.resolver.spec.ts
+++ src/templates/api/__module_name__/resolvers/__bounded_context_name__-update-__module_name__.resolver.spec.ts
@@ -1,11 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Test, TestingModule } from '@nestjs/testing';
+{{#if schema.properties.hasI18n}}
+import { CacheModule } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+{{/if}}
 
 // custom items
 import { {{ toPascalCase schema.boundedContextName }}Update{{ toPascalCase schema.moduleName }}Resolver } from './{{ toKebabCase schema.boundedContextName }}-update-{{ toKebabCase schema.moduleName }}.resolver';
 import { ICommandBus } from '{{ config.auroraLocalPackage }}/cqrs/domain/command-bus';
 import { IQueryBus } from '{{ config.auroraLocalPackage }}/cqrs/domain/query-bus';
-import { {{ toCamelCase schema.moduleNames }} } from '{{ config.applicationsContainer }}/{{ toKebabCase schema.boundedContextName }}/{{ toKebabCase schema.moduleName }}/infrastructure/seeds/{{ toKebabCase schema.moduleName }}.seed';
 import { {{ toPascalCase schema.boundedContextName }}Update{{ toPascalCase schema.moduleName }}Input } from './../../../../graphql';
+{{#if schema.properties.hasI18n}}
+import { AddI18NConstraintService } from '@apps/common/lang/application/shared/add-i18n-constraint.service';
+import { GetLangsCacheService } from '@apps/common/lang/application/shared/get-langs-cache.service';
+{{/if}}
+
+// sources
+{{#if schema.properties.hasI18n}}
+import { langs } from '@apps/common/lang/infrastructure/seeds/lang.seed';
+{{/if}}
+import { {{ toCamelCase schema.moduleNames }} } from '{{ config.applicationsContainer }}/{{ toKebabCase schema.boundedContextName }}/{{ toKebabCase schema.moduleName }}/infrastructure/seeds/{{ toKebabCase schema.moduleName }}.seed';
 
 describe('{{ toPascalCase schema.boundedContextName }}Update{{ toPascalCase schema.moduleName }}Resolver', () =>
 {
@@ -16,8 +30,28 @@ describe('{{ toPascalCase schema.boundedContextName }}Update{{ toPascalCase sche
     beforeAll(async () =>
     {
         const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                {{#if schema.properties.hasI18n}}
+                CacheModule.register(),
+                {{/if}}
+            ],
             providers: [
                 {{ toPascalCase schema.boundedContextName }}Update{{ toPascalCase schema.moduleName }}Resolver,
+                {{#if schema.properties.hasI18n}}
+                AddI18NConstraintService,
+                {
+                    provide : ConfigService,
+                    useValue: {
+                        get: (key: string) => key === 'APP_LANG' ? 'es' : ''
+                    }
+                },
+                {
+                    provide : GetLangsCacheService,
+                    useValue: {
+                        main: () => langs,
+                    }
+                },
+                {{/if}}
                 {
                     provide : IQueryBus,
                     useValue: {
