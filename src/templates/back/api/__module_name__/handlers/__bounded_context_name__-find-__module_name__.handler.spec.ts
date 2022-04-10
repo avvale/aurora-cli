3341cab22a2679f3c971fe4bd5cee7654ab87e56
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2022-04-10 22:55:28 +0200

    wip

---
+++ src/templates/back/api/__module_name__/handlers/__bounded_context_name__-find-__module_name__.handler.spec.ts
@@ -0,0 +1,87 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Test, TestingModule } from '@nestjs/testing';
+{{#if schema.properties.hasI18n}}
+import { CacheModule, CACHE_MANAGER } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+{{/if}}
+import { {{#if schema.properties.hasI18n}}AddI18NConstraintService, {{/if}}ICommandBus, IQueryBus } from '{{ config.auroraCorePackage }}';
+
+// custom items
+import { {{ toPascalCase schema.boundedContextName }}Find{{ toPascalCase schema.moduleName }}Handler } from './{{ toKebabCase schema.boundedContextName }}-find-{{ toKebabCase schema.moduleName }}.handler';
+
+// sources
+{{#if schema.properties.hasI18n}}
+import { langs } from '{{#eq schema.boundedContextName 'common'}}../../../../{{ config.applicationsContainer }}/common/lang/infrastructure/seeds/lang.seed{{else}}aurora-ts-common{{/eq}}';
+{{/if}}
+import { {{ toCamelCase schema.moduleNames }} } from '../../../../{{ config.applicationsContainer }}/{{ toKebabCase schema.boundedContextName }}/{{ toKebabCase schema.moduleName }}/infrastructure/seeds/{{ toKebabCase schema.moduleName }}.seed';
+
+describe('{{ toPascalCase schema.boundedContextName }}Find{{ toPascalCase schema.moduleName }}Handler', () =>
+{
+    let handler: {{ toPascalCase schema.boundedContextName }}Find{{ toPascalCase schema.moduleName }}Handler;
+    let queryBus: IQueryBus;
+    let commandBus: ICommandBus;
+
+    beforeAll(async () =>
+    {
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                {{#if schema.properties.hasI18n}}
+                CacheModule.register(),
+                {{/if}}
+            ],
+            providers: [
+                {{ toPascalCase schema.boundedContextName }}Find{{ toPascalCase schema.moduleName }}Handler,
+                {{#if schema.properties.hasI18n}}
+                AddI18NConstraintService,
+                {
+                    provide : ConfigService,
+                    useValue: {
+                        get: (key: string) => key === 'APP_LANG' ? 'es' : '',
+                    }
+                },
+                {
+                    provide : CACHE_MANAGER,
+                    useValue: {
+                        get: (key: string) => key === 'common/lang' ? langs : null,
+                    }
+                },
+                {{/if}}
+                {
+                    provide : IQueryBus,
+                    useValue: {
+                        ask: () => { /**/ },
+                    },
+                },
+                {
+                    provide : ICommandBus,
+                    useValue: {
+                        dispatch: () => { /**/ },
+                    },
+                },
+            ],
+        }).compile();
+
+        handler    = module.get<{{ toPascalCase schema.boundedContextName }}Find{{ toPascalCase schema.moduleName }}Handler>({{ toPascalCase schema.boundedContextName }}Find{{ toPascalCase schema.moduleName }}Handler);
+        queryBus    = module.get<IQueryBus>(IQueryBus);
+        commandBus  = module.get<ICommandBus>(ICommandBus);
+    });
+
+    test('{{ toPascalCase schema.boundedContextName }}Find{{ toPascalCase schema.moduleName }}Resolver should be defined', () =>
+    {
+        expect(handler).toBeDefined();
+    });
+
+    describe('main', () =>
+    {
+        test('{{ toPascalCase schema.boundedContextName }}Find{{ toPascalCase schema.moduleName }}Resolver should be defined', () =>
+        {
+            expect(handler).toBeDefined();
+        });
+
+        test('should return a {{ toCamelCase schema.moduleName }}', async () =>
+        {
+            jest.spyOn(queryBus, 'ask').mockImplementation(() => new Promise(resolve => resolve({{ toCamelCase schema.moduleNames }}[0])));
+            expect(await handler.main()).toBe({{ toCamelCase schema.moduleNames }}[0]);
+        });
+    });
+});
