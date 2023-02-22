bb69aff5962bb0913591e715f595b74a0faf9a11
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-02-22 21:41:20 +0100

    add admin navigation

--- src/templates/front/application/src/app/modules/admin/admin.navigation.ts
+++ src/templates/front/application/src/app/modules/admin/admin.navigation.ts
@@ -1,6 +1,3 @@
 import { FuseNavigationItem } from '@fuse/components/navigation';
-import { exampleNavigation } from './example/example.navigation';
-
 export const adminNavigation: FuseNavigationItem[] = [
-    exampleNavigation,
 ];
