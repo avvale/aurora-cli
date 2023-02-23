3a228ab7632bbf19f4b6e5366890e38f20950a7f
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-02-23 10:42:56 +0100

    wip

---
+++ src/templates/front/application/src/app/core/index.ts
@@ -0,0 +1,4 @@
+export { appConfig } from './config/app.config';
+export { AuthGuard } from './auth/guards/auth.guard';
+export { CoreModule } from './core.module';
+export { NoAuthGuard } from './auth/guards/noAuth.guard';
