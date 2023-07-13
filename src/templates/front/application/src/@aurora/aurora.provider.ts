4c7a8f0858daecdf105c92852c18cc5f24a0bad7
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-07-13 12:41:39 +0200

    wip

--- src/templates/front/application/src/@aurora/aurora.provider.ts
+++ src/templates/front/application/src/@aurora/aurora.provider.ts
@@ -1,17 +1,7 @@
 import { APP_INITIALIZER, EnvironmentProviders, Provider } from '@angular/core';
-import { AuroraGridManagerService, AuthenticationAuroraAdapterService, AuthenticationDisabledAdapterGuard, AuthenticationMockAdapterService, AuthenticationService, BootstrapService, COMPACT_NAVIGATION, DEFAULT_NAVIGATION, EnvironmentsInformationMockAdapterService, EnvironmentsInformationService, FUTURISTIC_NAVIGATION, GridManagerService, HORIZONTAL_NAVIGATION, IamAuroraAdapterService, IamMockAdapterService, IamService, SessionLocalStorageService, SessionService, UserMetaStorageLocalStorageAdapterService, UserMetaStorageService, compactNavigation, defaultNavigation, futuristicNavigation, horizontalNavigation, provideApollo, provideValidationMessages, translocoLoader } from '@aurora';
+import { AuroraGridManagerService, AuthenticationAuroraAdapterService, AuthenticationDisabledAdapterGuard, AuthenticationMockAdapterService, AuthenticationService, BootstrapService, COMPACT_NAVIGATION, DEFAULT_NAVIGATION, EnvironmentsInformationMockAdapterService, EnvironmentsInformationService, FUTURISTIC_NAVIGATION, GridManagerService, HORIZONTAL_NAVIGATION, IamAuroraAdapterService, IamMockAdapterService, IamService, SessionLocalStorageService, SessionService, UserMetaStorageLocalStorageAdapterService, UserMetaStorageService, compactNavigation, defaultNavigation, futuristicNavigation, horizontalNavigation, provideApollo, provideValidationMessages } from '@aurora';
 import { AuthGuard } from 'app/core/auth/guards/auth.guard';
-import { UserMetaStorageIamAdapterService } from 'app/modules/admin/apps/iam';
-
-import './prototypes/string-to-camel-case.interface';
-import './prototypes/string-to-camel-case';
-import './prototypes/string-to-kebab-case.interface';
-import './prototypes/string-to-kebab-case';
-import './prototypes/string-to-pascal-case.interface';
-import './prototypes/string-to-pascal-case';
-import './prototypes/string-to-snake-case.interface';
-import './prototypes/string-to-snake-case';
-
+import './aurora.prototypes';
 
 export const provideAurora = (): Array<Provider | EnvironmentProviders> =>
 {
