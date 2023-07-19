3416c60f6a011008306d8d7980de20a0c5190478
Carlos Palacín <carlos.rodriguez.palacin@gmail.com>
2023-07-19 17:09:11 +0200

    minor fix

--- src/templates/back/@api/__module_name__/controllers/__bounded_context_name__-find-__module_name__.controller.spec.ts
+++ src/templates/back/@api/__module_name__/controllers/__bounded_context_name__-find-__module_name__.controller.spec.ts
@@ -3,31 +3,22 @@
         array
             (object items=(array 'Test' 'TestingModule')  path='@nestjs/testing')
             (object items=(sumStrings (toCamelCase schema.boundedContextName) 'Mock' (toPascalCase schema.moduleName) 'Data') path=(sumStrings config.appContainer '/' (toKebabCase schema.boundedContextName) '/' (toKebabCase schema.moduleName)))
-            
             (object
                 items=
                 (
                     array
-                    (sumStrings (toPascalCase schema.boundedContextName) 'Find' (toPascalCase schema.moduleName) 'Controller')
-                    (sumStrings (toPascalCase schema.boundedContextName) 'Find' (toPascalCase schema.moduleName) 'Handler')
+                        (sumStrings (toPascalCase schema.boundedContextName) 'Find' (toPascalCase schema.moduleName) 'Controller')
+                        (sumStrings (toPascalCase schema.boundedContextName) 'Find' (toPascalCase schema.moduleName) 'Handler')
                 )
                 path=(sumStrings config.apiContainer '/' (toKebabCase schema.boundedContextName) '/' (toKebabCase schema.moduleName))
         )
     )
 ~}}
 {{#if schema.properties.hasI18n}}
-{{ push importsArray
-    (object items=(array 'CacheModule') path='@nestjs/cache-manager')
+{{ 
+    push importsArray
+        (object items=(array 'CacheModule') path='@nestjs/cache-manager')
 ~}}
-{{#eq schema.boundedContext 'common'}}
-{{ push importsArray
-    (object items=(array 'langs') path=(sumStrings config.appContrainer '/common/lang'))
-}}
-{{else}}
-{{ push importsArray
-    (object items=(array 'langs') path='@aurorajs.dev/common')
-}}
-{{/eq}}
 {{/if}}
 {{{ importManager (object imports=importsArray) }}}
 describe('{{ toPascalCase schema.boundedContextName }}Find{{ toPascalCase schema.moduleName }}Controller', () =>
