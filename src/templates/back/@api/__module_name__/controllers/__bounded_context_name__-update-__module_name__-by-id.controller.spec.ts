a4b699e19a16a03ac58e33f0ac4a2efb022edfac
Carlos Palacín <carlos.rodriguez.palacin@gmail.com>
2023-07-21 16:48:09 +0200

    minor fix

--- src/templates/back/@api/__module_name__/controllers/__bounded_context_name__-update-__module_name__-by-id.controller.spec.ts
+++ src/templates/back/@api/__module_name__/controllers/__bounded_context_name__-update-__module_name__-by-id.controller.spec.ts
@@ -7,26 +7,18 @@
                 items=
                 (
                     array
-                    (sumStrings (toPascalCase schema.boundedContextName) 'Update' (toPascalCase schema.moduleName) 'ByIdController')
-                    (sumStrings (toPascalCase schema.boundedContextName) 'Update' (toPascalCase schema.moduleName) 'ByIdHandler')
+                        (sumStrings (toPascalCase schema.boundedContextName) 'Update' (toPascalCase schema.moduleName) 'ByIdController')
+                        (sumStrings (toPascalCase schema.boundedContextName) 'Update' (toPascalCase schema.moduleName) 'ByIdHandler')
                 )
                 path=(sumStrings config.apiContainer '/' (toKebabCase schema.boundedContextName) '/' (toKebabCase schema.moduleName))
         )
     )
 ~}}
 {{#if schema.properties.hasI18n}}
-{{ push importsArray
-    (object items=(array 'CacheModule') path='@nestjs/cache-manager')
+{{
+    push importsArray
+        (object items=(array 'CacheModule') path='@nestjs/cache-manager')
 ~}}
-{{#eq schema.boundedContext 'common'}}
-    {{ push importsArray
-        (object items=(array 'langs') path=(sumStrings config.appContrainer '/common/lang'))
-    }}
-    {{else}}
-    {{ push importsArray
-        (object items=(array 'langs') path='@aurorajs.dev/common')
-    }}
-{{/eq}}
 {{/if}}
 {{{ importManager (object imports=importsArray) }}}
 describe('{{ toPascalCase schema.boundedContextName }}Update{{ toPascalCase schema.moduleName }}ByIdController', () =>
