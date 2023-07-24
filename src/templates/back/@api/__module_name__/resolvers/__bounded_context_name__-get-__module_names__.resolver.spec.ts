85bbea12aed9290b88553d64a04a96f368cb0412
Carlos Palacín <carlos.rodriguez.palacin@gmail.com>
2023-07-25 00:42:20 +0200

    delete import langs

--- src/templates/back/@api/__module_name__/resolvers/__bounded_context_name__-get-__module_names__.resolver.spec.ts
+++ src/templates/back/@api/__module_name__/resolvers/__bounded_context_name__-get-__module_names__.resolver.spec.ts
@@ -8,26 +8,18 @@
                 items=
                 (
                     array
-                    (sumStrings (toPascalCase schema.boundedContextName) 'Get' (toPascalCase schema.moduleNames) 'Resolver')
-                    (sumStrings (toPascalCase schema.boundedContextName) 'Get' (toPascalCase schema.moduleNames) 'Handler')
+                        (sumStrings (toPascalCase schema.boundedContextName) 'Get' (toPascalCase schema.moduleNames) 'Resolver')
+                        (sumStrings (toPascalCase schema.boundedContextName) 'Get' (toPascalCase schema.moduleNames) 'Handler')
                 )
                 path=(sumStrings config.apiContainer '/' (toKebabCase schema.boundedContextName) '/' (toKebabCase schema.moduleName))
         )
     )
 ~}}
 {{#if schema.properties.hasI18n}}
-{{ push importsArray
-    (object items=(array 'CacheModule') path='@nestjs/cache-manager')
+{{
+    push importsArray
+        (object items=(array 'CacheModule') path='@nestjs/cache-manager')
 ~}}
-{{#eq schema.boundedContext 'common'}}
-    {{ push importsArray
-        (object items=(array 'langs') path=(sumStrings config.appContrainer '/common/lang'))
-    }}
-    {{else}}
-    {{ push importsArray
-        (object items=(array 'langs') path='@aurorajs.dev/common')
-    }}
-{{/eq}}
 {{/if}}
 {{{ importManager (object imports=importsArray) }}}
 describe('{{ toPascalCase schema.boundedContextName }}Get{{ toPascalCase schema.moduleNames }}Resolver', () =>
