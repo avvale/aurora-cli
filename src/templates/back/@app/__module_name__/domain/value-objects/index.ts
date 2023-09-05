f6ca40340e9ccbaf658d800dbb8f3cd9d5328380
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-09-05 22:45:13 +0200

    delete imports many to many value objects

--- src/templates/back/@app/__module_name__/domain/value-objects/index.ts
+++ src/templates/back/@app/__module_name__/domain/value-objects/index.ts
@@ -11,19 +11,4 @@
 ~}}
 {{/if}}
 {{/each}}
-{{#each (getRelationshipManyToManyProperties schema.aggregateProperties) }}
-{{
-    push ../exportsArray
-        (
-            object
-                items=(sumStrings (toPascalCase ../schema.boundedContextName) (toPascalCase ../schema.moduleName) (toPascalCase ../schema.moduleName) (toPascalCase ../schema.moduleName) 'Id')
-                path=(sumStrings './' (toKebabCase ../schema.boundedContextName) '-' (toKebabCase ../schema.moduleName) '-' (toKebabCase ../schema.moduleName) '-' (toKebabCase ../schema.moduleName) '-id')
-        )
-        (
-            object
-                items=(sumStrings (toPascalCase ../schema.boundedContextName) (toPascalCase ../schema.moduleName) (toPascalCase ../schema.moduleName) (toPascalCase relationship.singularName) 'Id')
-                path=(sumStrings './' (toKebabCase ../schema.boundedContextName) '-' (toKebabCase ../schema.moduleName) '-' (toKebabCase ../schema.moduleName) '-' (toKebabCase relationship.singularName) '-id')
-        )
-~}}
-{{/each}}
 {{{ importManager (object imports=exportsArray commandType='export') }}}
