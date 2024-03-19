3d39972c4fedf5a7383b367a060ad7adfe206b10
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2024-03-19 23:36:07 +0100

    wip

---
+++ src/templates/front/application/src/@aurora/components/grid-select-element/grid-select-element.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { NgForOf } from '@angular/common';
+import { GridSelectElementComponent } from './grid-select-element.component';
+import { GridSelectElementCellValueTemplateDirective } from './directives/grid-select-element-cell-value-template.directive';
+import { GridSelectElementCustomHeaderTemplateDirective } from './directives/grid-select-element-custom-header-template.directive';
+
+@NgModule({
+    imports: [
+        GridSelectElementCellValueTemplateDirective,
+        GridSelectElementComponent,
+        GridSelectElementCustomHeaderTemplateDirective,
+        NgForOf,
+    ],
+    exports: [
+        GridSelectElementCellValueTemplateDirective,
+        GridSelectElementComponent,
+        GridSelectElementCustomHeaderTemplateDirective,
+        NgForOf,
+    ],
+})
+export class GridSelectElementsModule { }
--- src/templates/front/application/src/@aurora/components/grid-select-element/public-api.ts
+++ src/templates/front/application/src/@aurora/components/grid-select-element/public-api.ts
@@ -1,3 +1,4 @@
 export * from '@aurora/components/grid-select-element/directives/grid-select-element-cell-value-template.directive';
 export * from '@aurora/components/grid-select-element/directives/grid-select-element-custom-header-template.directive';
 export * from '@aurora/components/grid-select-element/grid-select-element.component';
+export * from '@aurora/components/grid-select-element/grid-select-element.module';
