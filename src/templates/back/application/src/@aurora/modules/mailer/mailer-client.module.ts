067154503c357230200fdb5fc29bf17875d8408d
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2024-03-19 23:21:24 +0100

    wip

--- src/templates/back/application/src/@aurora/modules/mailer/mailer-client.module.ts
+++ src/templates/back/application/src/@aurora/modules/mailer/mailer-client.module.ts
@@ -1,37 +1,52 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as handlebarsHelpers from 'handlebars-helpers';
 
-@Module({
-    imports: [
-        MailerModule.forRootAsync({
-            imports   : [ConfigModule],
-            inject    : [ConfigService],
-            useFactory: (configService: ConfigService) => ({
-                transport: {
-                    host     : configService.get<string>('MAILER_HOST'),
-                    port     : +configService.get<number>('MAILER_PORT'),
-                    ignoreTLS: configService.get<string>('MAILER_IGNORE_TLS') === 'true' ? true : false,
-                    secure   : configService.get<string>('MAILER_SECURE') === 'true' ? true : false,
-                    auth     : {
-                        user: configService.get<string>('MAILER_USER'),
-                        pass: configService.get<string>('MAILER_PASSWORD'),
-                    },
-                },
-                defaults: {
-                    from: configService.get<string>('MAILER_FROM'),
-                },
-                template: {
-                    dir    : __dirname + '/templates',
-                    adapter: new HandlebarsAdapter({ ...handlebarsHelpers() }),
-                    options: {
-                        strict: true,
-                    },
-                },
-            }),
-        }),
-    ],
-})
-export class MailerCLientModule { }
+@Module({})
+export class MailerCLientModule
+{
+    public static forRootAsync(): DynamicModule
+    {
+        if (process.env.MAILER_ENABLED !== 'true')
+        {
+            return {
+                module: MailerCLientModule,
+            };
+        }
+
+        return {
+            module : MailerCLientModule,
+            imports: [
+                /** Modules **/
+                MailerModule.forRootAsync({
+                    imports   : [ConfigModule],
+                    inject    : [ConfigService],
+                    useFactory: (configService: ConfigService) => ({
+                        transport: {
+                            host     : configService.get<string>('MAILER_HOST'),
+                            port     : +configService.get<number>('MAILER_PORT'),
+                            ignoreTLS: configService.get<string>('MAILER_IGNORE_TLS') === 'true' ? true : false,
+                            secure   : configService.get<string>('MAILER_SECURE') === 'true' ? true : false,
+                            auth     : {
+                                user: configService.get<string>('MAILER_USER'),
+                                pass: configService.get<string>('MAILER_PASSWORD'),
+                            },
+                        },
+                        defaults: {
+                            from: configService.get<string>('MAILER_FROM'),
+                        },
+                        template: {
+                            dir    : __dirname + '/templates',
+                            adapter: new HandlebarsAdapter({ ...handlebarsHelpers() }),
+                            options: {
+                                strict: true,
+                            },
+                        },
+                    }),
+                }),
+            ],
+        };
+    }
+}
