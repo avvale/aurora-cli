82b91067afd52ec4cf402f338c96ff341bb333f7
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2025-06-06 12:12:00 +0200

    delete azure storage account

--- src/templates/back/packages/azure-storage-account/src/@api/azure-storage-account/blob/shared/public-api.ts
+++
@@ -1,3 +0,0 @@
-export * from './services/azure-storage-account-copy-blob.service';
-export * from './services/azure-storage-account-get-base64-from-blob.service';
-export * from './services/azure-storage-account-upload-blob.service';
