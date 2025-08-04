4a0413f1038e5acee891ee226340e1ab14e14c28
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2025-08-04 10:27:15 +0200

    feat: Refactor component configurations to remove standalone property and optimize observable initialization

--- src/templates/back/application/commitlint.config.js
+++ src/templates/back/application/commitlint.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    rules: {
+        'header-max-length': [2, 'always', 120],
+    },
     // ignore commit messages that are just version numbers
     // e.g., "v1.0.0"
     ignores: [
