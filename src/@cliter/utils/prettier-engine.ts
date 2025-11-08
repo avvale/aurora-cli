150eef8294b6c19bd8471e83c22f4d8baea2b8b3
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2025-11-08 11:15:19 +0100

    fix: refactor prettier-engine to streamline code formatting and improve plugin handling; update service template to include optional scope parameter

--- src/@cliter/utils/prettier-engine.ts
+++ src/@cliter/utils/prettier-engine.ts
@@ -1,10 +1,10 @@
+import * as chalk from 'chalk';
 import { createRequire } from 'node:module';
 import { join } from 'node:path';
-import * as chalk from 'chalk';
 
 export async function formatCode(
     code: string,
-    filePath: string,
+    filepath: string,
     consumerCwd: string,
 ): Promise<string>
 {
@@ -13,31 +13,8 @@ export async function formatCode(
         const requireFromConsumer = createRequire(join(consumerCwd, 'package.json'));
         const prettier = requireFromConsumer('prettier');
 
-        const plugins: any[] = [];
-        const tryLoad = (id: string) =>
-        {
-            try
-            {
-                plugins.push(requireFromConsumer(id));
-            }
-            catch
-            {
-                //  console.log('%s Prettier plugin %s not found in consumer project', chalk.yellow.bold('[INFO]'), chalk.cyan(id));
-            }
-        };
-
-        // core parsers (TS/HTML/ESTree)
-        tryLoad('prettier/plugins/typescript');
-        tryLoad('prettier/plugins/html');
-        tryLoad('prettier/plugins/estree');
-
-        // angular plugins
-        tryLoad('prettier-plugin-tailwindcss');
-        tryLoad('prettier-plugin-organize-attributes');
-        tryLoad('prettier-plugin-organize-imports');
-
         // load config project
-        const resolved = (await prettier.resolveConfig(filePath, { editorconfig: true })) ?? {};
+        const resolved = (await prettier.resolveConfig(filepath, { editorconfig: true })) ?? {};
 
         // format using your config + filepath (for heuristics and parser)
         return await prettier
@@ -45,10 +22,10 @@ export async function formatCode(
                 code,
                 {
                     ...resolved,
-                    filepath: filePath,
-                    plugins,
+                    filepath,
+                    pluginSearchDirs: [consumerCwd],
                     // to force parser according extension:
-                    // parser: filePath.endsWith('.ts') ? 'typescript' : 'html',
+                    // parser: filepath.endsWith('.ts') ? 'typescript' : 'html',
                 },
             );
     }
