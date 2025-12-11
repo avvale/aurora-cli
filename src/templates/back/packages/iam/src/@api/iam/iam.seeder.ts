b21dad94a6b8d4851e5730d2fcf988cff802578d
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2025-12-11 18:40:44 +0100

    feat: enhance IamSeeder and iam.seed with additional permissions, roles, and tenants; refactor code for consistency

--- src/templates/back/packages/iam/src/@api/iam/iam.seeder.ts
+++ src/templates/back/packages/iam/src/@api/iam/iam.seeder.ts
@@ -1,19 +1,30 @@
 // ignored file
-import { Injectable } from '@nestjs/common';
+import {
+    accounts,
+    boundedContexts,
+    permissions,
+    roles,
+    tenants,
+    users,
+} from '@app/iam/iam.seed';
 import { ICommandBus, IQueryBus } from '@aurorajs.dev/core';
-import { accounts, boundedContexts, permissions, roles, users } from '@app/iam/iam.seed';
+import { Injectable } from '@nestjs/common';
 
 // sources
+import {
+    IamAccount,
+    IamCreateAccountsCommand,
+    IamFindAccountByIdQuery,
+} from '@app/iam/account';
 import { IamBoundedContextHelper } from '@app/iam/bounded-context';
 import { IamPermissionHelper } from '@app/iam/permission';
-import { IamAccount, IamCreateAccountsCommand, IamFindAccountByIdQuery } from '@app/iam/account';
-import { IamCreateUsersCommand } from '@app/iam/user';
 import { IamCreateRolesCommand } from '@app/iam/role';
 import { IamCreateRolesAccountsCommand } from '@app/iam/role-account';
+import { IamCreateTenantsCommand } from '@app/iam/tenant';
+import { IamCreateUsersCommand } from '@app/iam/user';
 
 @Injectable()
-export class IamSeeder
-{
+export class IamSeeder {
     administratorAccount: IamAccount;
 
     constructor(
@@ -21,47 +32,62 @@ export class IamSeeder
         private readonly queryBus: IQueryBus,
     ) {}
 
-    async main(): Promise<boolean>
-    {
-        try
-        {
+    async main(): Promise<boolean> {
+        try {
             this.administratorAccount = await this.queryBus.ask(
                 new IamFindAccountByIdQuery(
                     IamPermissionHelper.administratorAccountId,
                 ),
             );
-        }
-        catch (error)
-        {
+        } catch (error) {
             // avoid error 404
-            if (error.response.statusCode === 404) { /**/ }
+            if (error?.response?.statusCode === 404) {
+                /**/
+            }
         }
 
-        if (this.administratorAccount)
-        {
+        if (this.administratorAccount) {
             // create bounded contexts and permissions
-            await IamBoundedContextHelper.createBoundedContexts(this.commandBus, boundedContexts);
-            await IamPermissionHelper.createPermissions(this.commandBus, this.queryBus, permissions);
-        }
-        else
-        {
-            await this.commandBus.dispatch(new IamCreateAccountsCommand(accounts));
+            await IamBoundedContextHelper.createBoundedContexts(
+                this.commandBus,
+                boundedContexts,
+            );
+            await IamPermissionHelper.createPermissions(
+                this.commandBus,
+                this.queryBus,
+                permissions,
+            );
+        } else {
+            await this.commandBus.dispatch(
+                new IamCreateTenantsCommand(tenants),
+            );
+            await this.commandBus.dispatch(
+                new IamCreateAccountsCommand(accounts),
+            );
             await this.commandBus.dispatch(new IamCreateUsersCommand(users));
             await this.commandBus.dispatch(new IamCreateRolesCommand(roles));
 
             // set all roles to administration account
-            const rolesAccounts = roles.map(role =>
-            {
+            const rolesAccounts = roles.map((role) => {
                 return {
-                    roleId   : role.id,
+                    roleId: role.id,
                     accountId: IamPermissionHelper.administratorAccountId,
                 };
             });
-            await this.commandBus.dispatch(new IamCreateRolesAccountsCommand(rolesAccounts));
+            await this.commandBus.dispatch(
+                new IamCreateRolesAccountsCommand(rolesAccounts),
+            );
 
             // create bounded contexts and permissions
-            await IamBoundedContextHelper.createBoundedContexts(this.commandBus, boundedContexts);
-            await IamPermissionHelper.createPermissions(this.commandBus, this.queryBus, permissions);
+            await IamBoundedContextHelper.createBoundedContexts(
+                this.commandBus,
+                boundedContexts,
+            );
+            await IamPermissionHelper.createPermissions(
+                this.commandBus,
+                this.queryBus,
+                permissions,
+            );
         }
 
         return true;
