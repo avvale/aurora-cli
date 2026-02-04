b996c0d04991ed1a1c1b03f17a41e1f087d38dcc
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2026-02-04 23:20:16 +0100

    refactor: improve formatting and readability in indexesManager helper

--- src/@cliter/handlebars/helpers/indexes-manager.ts
+++ src/@cliter/handlebars/helpers/indexes-manager.ts
@@ -1,49 +1,46 @@
 import * as handlebars from 'handlebars';
-import * as _ from 'lodash';
 import { Property } from '../../types';
 
-handlebars.registerHelper('indexesManager', function(
+handlebars.registerHelper(
+  'indexesManager',
+  function (
     {
-        indexProperties = [],
-        isI18n = false,
+      indexProperties = [],
+      isI18n = false,
     }: {
-        indexProperties: Property[];
-        isI18n: boolean;
+      indexProperties: Property[];
+      isI18n: boolean;
     },
     context,
-)
-{
+  ) {
     let response = '';
 
-    for (const [i, indexProperty] of indexProperties.entries())
-    {
-        response += '\t\t{\n';
-        response += '\t\t\tfields: [';
+    for (const [i, indexProperty] of indexProperties.entries()) {
+      response += '\t\t{\n';
+      response += '\t\t\tfields: [';
 
-        if (Array.isArray(indexProperty.indexFields))
-        {
-            for (const [j, indexField] of indexProperty.indexFields.entries())
-            {
-                if (j === indexProperty.indexFields.length - 1)
-                {
-                    response += `'${indexField}'`;
-                    continue;
-                }
+      if (Array.isArray(indexProperty.indexFields)) {
+        for (const [j, indexField] of indexProperty.indexFields.entries()) {
+          if (j === indexProperty.indexFields.length - 1) {
+            response += `'${indexField}'`;
+            continue;
+          }
 
-                response += `'${indexField}', `;
-            }
+          response += `'${indexField}', `;
         }
-        else
-        {
-            response += `'${indexProperty.name}'`;
-        }
+      } else {
+        response += `'${indexProperty.name}'`;
+      }
 
-        response += '],\n';
-        response += `\t\t\tunique: ${indexProperty.index === 'unique' ? 'true' : 'false'},\n`;
-        if (indexProperty.indexName) response += `\t\t\tname: '${indexProperty.indexName}',\n`;
-        if (indexProperty.indexUsing) response += `\t\t\tusing: '${indexProperty.indexUsing}',\n`;
-        response += `\t\t},${i === indexProperties.length - 1 ? '' : '\n'}`;
+      response += '],\n';
+      response += `\t\t\tunique: ${indexProperty.index === 'unique' ? 'true' : 'false'},\n`;
+      if (indexProperty.indexName)
+        response += `\t\t\tname: '${indexProperty.indexName}',\n`;
+      if (indexProperty.indexUsing)
+        response += `\t\t\tusing: '${indexProperty.indexUsing}',\n`;
+      response += `\t\t},${i === indexProperties.length - 1 ? '' : '\n'}`;
     }
 
     return response;
-});
+  },
+);
